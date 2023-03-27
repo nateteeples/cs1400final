4d915b10aafd9b8ef12190cb95ff83d398bceb13
nateteeples <25nteeples@go.dsdmail.net>
2023-03-26 20:48:39 -0600

    numpy fix

--- rooms.py
+++ rooms.py
@@ -1,3 +1,5 @@
+from numpy import array
+
 MOVEMENTS = {'north': array([0, 1, 0]), 'south': array([0, -1, 0]), 'east': array([-1, 0, 0]), 'west': array([1, 0, 0]), 'up': array([0, 0, 1]), 'down': array([0, 0, -1])}
 
 class Room():
